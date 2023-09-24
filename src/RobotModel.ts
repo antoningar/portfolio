387975cfa817561c9f66165df653e2a6d15073a8
antoningar <antoningaranto@protonmail.com>
2023-09-25 01:56:40 +0200

    feat: add modal

--- src/RobotModel.ts
+++ src/RobotModel.ts
@@ -3,30 +3,42 @@ import * as THREE from "three";
 export class RobotModel {
     group: THREE.Group;
 
-    // number: step indice
     // string: axe
     // number: trarget coords
     // number: end rotation
-    plan: [number, string, number, number][];
+    plan: [string, number, number][];
     step: number = 0;
+
     name: string;
+    description: string;
 
     basePosition: any;
 
-    constructor (group: THREE.Group, plan: [number, string, number, number][], name: string){
-        this.group = group;
-        this.plan = plan;
-        this.name = name;
-        this.basePosition = Object.assign({}, this.group.position);
+    isMoving: boolean = true;
+
+    constructor (
+        group: THREE.Group,
+        plan: [string, number, number][],
+        name: string,
+        description: string ){
+            this.group = group;
+            this.plan = plan;
+            this.name = name;
+            this.description = description;
+            this.basePosition = Object.assign({}, this.group.position);
     }
 
     move(): void {
-        let currentPlan: [number, string, number, number] = this.plan[this.step];
-        let currentPositionAxe: number = (currentPlan[1] === 'z') ? this.group.position.z : this.group.position.x;
-        let basePositionAxe: number = (currentPlan[1] === 'z') ? this.basePosition.z : this.basePosition.x;
+        if (!this.isMoving){
+            return;
+        }
 
-        if (Math.abs(Math.round(basePositionAxe) - Math.round(currentPositionAxe)) == Math.round(currentPlan[2])){
-            this.group.rotateY(currentPlan[3] * Math.PI / 180);
+        let currentPlan: [string, number, number] = this.plan[this.step];
+        let currentPositionAxe: number = (currentPlan[0] === 'z') ? this.group.position.z : this.group.position.x;
+        let basePositionAxe: number = (currentPlan[0] === 'z') ? this.basePosition.z : this.basePosition.x;
+
+        if (Math.abs(Math.round(basePositionAxe) - Math.round(currentPositionAxe)) == Math.round(currentPlan[1])){
+            this.group.rotateY(currentPlan[2] * Math.PI / 180);
             this.step++;
             if (this.step == this.plan.length){
                 this.step = 0;
@@ -37,4 +49,40 @@ export class RobotModel {
         
         this.group.translateZ(.05);
     }
+
+    getFaceCameraValues(): [any, any]{
+        const RECOIL: number = 20;
+
+        let currentPlan: [string, number, number] = this.plan[this.step];
+        let cameraPosition = {
+            x: this.basePosition.x,
+            y: this.basePosition.y,
+            z: this.basePosition.z
+        };
+        let cameraDirection = {
+            x: this.basePosition.x,
+            y: this.basePosition.y + 7,
+            z: this.basePosition.z
+        };
+        if (currentPlan[0] === 'z') {
+            //deal with z
+            if (this.group.position.z < this.basePosition.z) {
+                cameraPosition.z -= RECOIL;
+
+            } else {
+                cameraPosition.z += RECOIL;
+            }
+        }
+        else {
+            //deal wit x
+            if (this.group.position.x < this.basePosition.x) {
+                cameraPosition.x -= RECOIL;
+                
+            } else {
+                cameraPosition.x += RECOIL;
+            }
+        }
+
+        return [cameraPosition, cameraDirection];
+    };
 }
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,43 @@
 import * as THREE from "three";
-
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 import { RobotModel } from "./RobotModel.ts";
+
+const currentWindow = window as any
+const gsap = currentWindow.gsap;
 
 let container, clock: THREE.Clock, mixer: THREE.AnimationMixer;
 let camera: THREE.PerspectiveCamera, scene: THREE.Scene, renderer: THREE.WebGLRenderer, model;
 let models: RobotModel[] = [];
 
-
+let danceAnimation: THREE.AnimationClip;
+let walkClip: THREE.AnimationClip;
 
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
 
+let robotFocus: RobotModel | null;
+let divModal: HTMLDivElement = document.createElement('div');
+let divLabel: HTMLDivElement = document.createElement('div');
 
+//x=width(left/right)
+//y=height(top/bottom)
+//z=zoom(in/out)
+const CameraInitialPosition = {
+  x: 25,
+  y: 20,
+  z: 40
+}
+
+const CameraInitialDirection = {
+  x: 0,
+  y: 2,
+  z: 0
+}
 
 init();
 animate();
+initModal();
 
 function init() {
   container = document.createElement("div");
@@ -28,8 +49,14 @@ function init() {
     0.25,
     100
   );
-  camera.position.set(25, 20, 40);
-  camera.lookAt(0, 2, 0);
+  camera.position.set(
+    CameraInitialPosition.x,
+    CameraInitialPosition.y,
+    CameraInitialPosition.z);
+  camera.lookAt(
+    CameraInitialDirection.x,
+    CameraInitialDirection.y,
+    CameraInitialDirection.z);
 
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xe0e0e0);
@@ -46,10 +73,10 @@ function init() {
   dirLight.position.set(0, 20, 10);
   scene.add(dirLight);
 
-  const grid = new THREE.GridHelper(200, 80, 0x000000, 0x000000);
-  grid.material.opacity = 0.2;
-  grid.material.transparent = true;
-  scene.add(grid);
+  // const grid = new THREE.GridHelper(200, 80, 0x000000, 0x000000);
+  // grid.material.opacity = 0.2;
+  // grid.material.transparent = true;
+  // scene.add(grid);
 
   // model
   const loader = new GLTFLoader();
@@ -58,18 +85,21 @@ function init() {
     function (gltf) {
       model = gltf.scene;
       scene.add(model);
-
-      let walkClip = gltf.animations.find(a => a.name === "Walking")!
-      launchWalking(model, walkClip);
+      danceAnimation = gltf.animations.find(a => a.name === "Dance")!
+      
+      walkClip = gltf.animations.find(a => a.name === "Walking")! 
+      launchAnimationClip(model, walkClip);
 
       const name: string = gltf.parser.json.skins[0].name;
-      let plan: [number, string, number, number][] = [
-        [0, "z", 10, 90],
-        [1, "x", 10, 90],
-        [0, "z", 10, 90],
-        [1, "x", 10, 90],
+      let plan: [string, number, number][] = [
+        ["z", 10, 90],
+        ["x", 10, 90],
+        ["z", 10, 90],
+        ["x", 10, 90],
       ]
-      models.push(new RobotModel(model ,plan, name))
+
+      const text: string = "Hello!\nMy name is Antonin, I'm into computing since 2016.\nI did a lot of different things so click on any other robots to learn more about me ! ";
+      models.push(new RobotModel(model ,plan, name, text))
     },
     undefined,
     function (e) {
@@ -85,7 +115,31 @@ function init() {
   window.addEventListener("resize", onWindowResize);
 }
 
-function launchWalking(model: THREE.Group, clip: THREE.AnimationClip){
+function initModal() {
+  divModal.className = "label-modal";
+  divModal.style.display = "none";
+
+  let divContainer: HTMLDivElement = document.createElement('div');
+  divContainer.className = "label-container";
+
+  divLabel.className = "label";
+
+  let divOutline: HTMLDivElement = document.createElement('div');
+  divOutline.className = "label-outline";
+
+  let btnClosed: HTMLButtonElement = document.createElement('button');
+  btnClosed.innerHTML = "CLOSE X";
+  btnClosed.className = "label-close";
+  btnClosed.onclick = onClosed;
+
+  divOutline.appendChild(divLabel);
+  divContainer.appendChild(divOutline);
+  divModal.appendChild(divContainer);
+  divModal.appendChild(btnClosed);
+  document.body.appendChild(divModal);
+}
+
+function launchAnimationClip(model: THREE.Group, clip: THREE.AnimationClip){
   mixer = new THREE.AnimationMixer(model);
   const action = mixer.clipAction(clip);
   action.clampWhenFinished = true;
@@ -95,12 +149,10 @@ function launchWalking(model: THREE.Group, clip: THREE.AnimationClip){
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function animate() {
-
   const dt = clock.getDelta();
   if (mixer) mixer.update(dt);
 
@@ -112,9 +164,11 @@ function animate() {
   });
 }
 
-
-
 function click(event: any) {
+  if (!focus) {
+    
+  }
+
 	// calculate pointer position in normalized device coordinates
 	// (-1 to +1) for both components
 	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
@@ -151,8 +205,45 @@ function getRobotName(obj: THREE.Object3D<THREE.Event>): string {
   return "";
 }
 
+function onClosed() {
+  moveCamera(CameraInitialPosition, CameraInitialDirection);
+  divModal.style.display = "none";
+  robotFocus!.isMoving = true;
+  launchAnimationClip(robotFocus!.group, walkClip);
+  
+  robotFocus = null;
+}
+
+function printDescription(description: string) {
+  divModal.style.display = "block";
+  divLabel.innerHTML = description;
+}
+
+function moveCamera(position: any, direction: any) {
+  gsap.to(camera.position, {
+    x: position.x,
+    y: position.y,
+    z: position.z,
+    duration: 1, 
+    onUpdate: function() {
+      camera.lookAt(
+        direction.x,
+        direction.y,
+        direction.z);
+    }
+  })
+}
+
 function onClickRobot(robotName: string) {
-  robot: RobotModel: models.find(r => r.name === robotName);
+  const robot: RobotModel = models.find(r => r.name === robotName)!;
+  robot.isMoving = false;
+
+  const [position, direction] = robot.getFaceCameraValues();
+  moveCamera(position, direction);
+  launchAnimationClip(robot.group, danceAnimation);
+  printDescription(robot.description);
+  
+  robotFocus = robot;
 }
 
 window.addEventListener( 'click', click);
