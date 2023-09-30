c6732fb40861f651a7abd8e9aa221ac5d45910a6
antoningar <antoningaranto@protonmail.com>
2023-09-30 18:03:01 +0200

    feat: add robots

--- src/robots.ts
+++ src/robots.ts
@@ -1,6 +1,6 @@
 export const ROBOTS: any = [
     {
-        filename: "/discordrobot.glb",
+        filename: "/merobot.glb",
         basePosition: {
             x: 0, y: 0, z: 0
         },
@@ -10,7 +10,7 @@ export const ROBOTS: any = [
             ["z", 10, 90],
             ["x", 10, 90],
             ],
-        text: "Hello!\nMy name is Antonin, I'm into computing since 2016.\nI did a lot of different things so click on any other robots to learn more about me !"
+        text: "Hello!\nMy name is Antonin, I'm into computing since 2016.\nI'm a backend developer in love with the craft part of my job. I'm able to build lot of different apps type, click on any other robots to learn more about me !\nYou can contact me at:\nantoningaranto@protonmail.com"
     },
     {
         filename: "/studentrobot.glb",
@@ -24,5 +24,57 @@ export const ROBOTS: any = [
             ["x", 10, -90],
             ],
         text: "I have two degree in computing :\nDUT in computing science in Bayonne, 2018\nSoftware engineer from ENSICAEN in Caen, 2021."
-    }
+    },
+    {
+        filename: "/workrobot.glb",
+        basePosition: {
+            x: -10, y: 0, z: -10
+        },
+        plan: [
+            ["z", 10, -90],
+            ["x", 10, -90],
+            ["z", 10, -90],
+            ["x", 10, -90],
+            ],
+        text: "My precedent jobs was :\n - <a href=\"https://lumiscaphe.com/\">Lumiscaphe</a>, Bordeaux, 2018: 3 months internship (video encoding, c), 3 months developer (end-to-end testing, SikuliX).\n - CGI, Bordeaux 2018 -2021: Apprentiship, 1 year VBA developer (VBA, SQL), 2 years web developer (JAVA Spring/Angular).\n - CGI, Rennes 2021 -2023:\nGenesys Cloud developer, integration (Terraform), custom web apps (.NET microservices, React web interface)"
+    },
+    {
+        filename: "/projectrobot.glb",
+        basePosition: {
+            x: 10, y: 0, z: -10
+        },
+        plan: [
+            ["z", 10, -90],
+            ["x", 10, -90],
+            ["z", 10, -90],
+            ["x", 10, -90],
+            ],
+        text: "I spend a lot of my free time coding.\nI've developed a variety of apps : API, website, bots...\nYou can find them on my <a href=\"https://github.com/antoningar\">GitHub profile<\a>"
+    },
+    {
+        filename: "/discordrobot.glb",
+        basePosition: {
+            x: -10, y: 0, z: 10
+        },
+        plan: [
+            ["z", 10, -90],
+            ["x", 10, -90],
+            ["z", 10, -90],
+            ["x", 10, -90],
+            ],
+        text: "Recently, I love spend time into building discord bot. My  biggest ones are :\n - Rolang Gamos, a game created by <a href=\"https://www.youtube.com/watch?v=FYklBCCaBmg&t=922s\">Rap Jeu</a>, here is the code : <a href=\"https://github.com/antoningar/BotRapJeu\">BotRapJeu</a>\n - BotGeoguessr, for playing at geoguessr in multiplayer at anytime with a premium account managed by the bot, code : <a href=\"https://github.com/antoningar/BotGeoguessr\">BotGeoguessr</a>\n - Gptbot, an interface for Chat GPT on discord : <a href=\"https://github.com/antoningar/GptBot/tree/develop\">GptBot</a>"
+    },
+    {
+        filename: "/futurrobot.glb",
+        basePosition: {
+            x: 20, y: 0, z: 0
+        },
+        plan: [
+            ["z", 10, 90],
+            ["x", 10, 90],
+            ["z", 10, 90],
+            ["x", 10, 90],
+            ],
+        text: "Now, my priority is  in the craftsmanship aspect of my work.\nI want my apps to be crafted to the highest standard possible.\nTo achieve this goal, I'm experimenting with various development processes such as TDD and BDD, as well as different architectures like hexagonal and clean architecture to find my favorite one.\nYou can contact me at:\nantoningaranto@protonmail.com"
+    },
 ]
